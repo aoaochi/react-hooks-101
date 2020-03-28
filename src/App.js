4aa597f016b9d49d4508660ac5426d97c49e4207
Masachika Aochi <aochi.masachika@gmail.com>
2020-03-28 18:49:12 +0900

    my first React Hook with useState

--- src/App.js
+++ src/App.js
@@ -1,10 +1,44 @@
-import React from 'react'
+import React,{ useState } from 'react'
 
 const App = () => {
+
+ const [count, setCount] = useState(0)
+
+  const increment = () => setCount (count +1)
+  const decrement = () => setCount (count -1)
+
+  const increment2 = () => setCount (previciousCount => previciousCount + 1)
+  const decrement2 = () => setCount (previciousCount => previciousCount - 1)
+
+  const reset = () => setCount (0)
+  const twice = () => setCount (previciousCount => previciousCount *2)
+  const calculate3 = () => setCount (previciousCount => {
+    if (previciousCount%3 === 0) {
+      previciousCount = previciousCount/3;
+    }
+    return previciousCount;
+  })
+  
+
   return (
-    <div>
-	This is template file
-    </div>
+    <React.Fragment> 
+      <div>
+	count: {count}
+      </div>
+      <div>
+        <button onClick={increment}>+1</button>
+        <button onClick={decrement}>-1</button>
+      </div>
+      <div>
+        <button onClick={increment2}>+1</button>
+        <button onClick={decrement2}>-1</button>
+      </div>
+      <div>
+        <button onClick={reset}>Reset</button>
+        <button onClick={twice}>*2</button>
+        <button onClick={calculate3}>3の倍数のときだけ3で割る</button>
+      </div>
+    </React.Fragment>
   )
 }
 
